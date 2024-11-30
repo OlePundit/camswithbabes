7f07a43f3efc33306be16b25d19db2291e5ad9b9
OlePundit <glennomondi@yahoo.com>
2024-11-30 14:22:06 +0300

    models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $models = Profile::all();
+        return view('home', compact('models'));
     }
     public function profile()
     {
