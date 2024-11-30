83657166ddaed0e0a609a2ddf2d24787f9472497
OlePundit <glennomondi@yahoo.com>
2024-11-30 15:58:59 +0300

    profile route

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -8,7 +8,7 @@
                 <h1 class="card-header">
                     {{$profile->name}}
                   </h1>
-                <img src="storage/{{ asset('storage/'.$profile->thumb) }}" class="card-img-top" alt="...">
+                <img src="{{ asset('storage/'.$profile->thumb) }}" class="card-img-top" alt="...">
                 <div class="card-body">
                   <p class="card-text">{{$profile->availability}}</p>
                 </div>
