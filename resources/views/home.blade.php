af29dd3cc4c63e95ad6e203343c18cafe7a938e3
OlePundit <glennomondi@yahoo.com>
2024-12-04 01:31:52 +0300

    overlay background

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -199,10 +199,19 @@
             <div id="popup-overlay"></div>
 
             <!-- Pop-up Content -->
+            <div style="background: url('{{url("/storage/images/dark-sexy.jpg")}}') center center / cover no-repeat;" >
+
             <div id="popup">
-              <h3>Welcome!</h3>
-              <p>This is an automatic pop-up message.</p>
-              <button id="close-popup">Close</button>
+                <img src="storage/images/logo1.png">
+              <h3>FIRST TOP UP OFFER!</h3>
+              <h4>50% EXTRA FREE CREDIT</h4>
+              <p>On first top-up</p>
+                <div class="row">
+                    <button id="close-popup"><a href="/register">Register</a></button>
+
+                    <button id="close-popup">Close</button>
+
+                </div>
             </div>
         
             <div class="grid_4">
